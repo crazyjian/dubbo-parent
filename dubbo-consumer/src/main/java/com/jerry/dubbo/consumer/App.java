15d89de83d23959042c8b3396bcd0d3a547e964c
jerry <1076441760@qq.com>
2017-11-08 12:25:04 +0800

    springBoot+dubbo

--- dubbo-consumer/src/main/java/com/jerry/dubbo/consumer/App.java
+++ dubbo-consumer/src/main/java/com/jerry/dubbo/consumer/App.java
@@ -4,8 +4,16 @@ import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.alibaba.dubbo.rpc.RpcContext;
 import com.evcorp.service.demo.DemoService;
 import com.jerry.dubbo.api.UserService;
@@ -14,12 +22,26 @@ import com.jerry.dubbo.api.UserService;
  * Hello world!
  *
  */
+@ImportResource("classpath:consumer.xml")
+@Controller
+@SpringBootApplication
 public class App 
 {
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping("/say")  
+	@ResponseBody
+    public String sayHello() throws InterruptedException, ExecutionException {  
+		String str = userService.sayHello("jim");
+		Future<String> f = RpcContext.getContext().getFuture();
+        return "消费方：" + f.get();  
+    }  
+	
     public static void main( String[] args ) throws IOException, InterruptedException, ExecutionException
     {
     	 //测试常规服务
-        ClassPathXmlApplicationContext context =
+      /*  ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("consumer.xml");
         context.start();
         System.out.println("consumer start");
@@ -30,6 +52,7 @@ public class App
         	String result = userService.sayHello("jim");
         	Future<String> f = RpcContext.getContext().getFuture();
         	System.out.println(f.get());
-        }
+        }*/
+    	SpringApplication.run(App.class, "--server.port=8080");
     }
 }
--- dubbo-provider/src/main/java/com/jerry/dubbo/provider/Provider.java
+++ dubbo-provider/src/main/java/com/jerry/dubbo/provider/Provider.java
@@ -2,15 +2,32 @@ package com.jerry.dubbo.provider;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jerry.dubbo.api.UserService;
 
 @ImportResource("classpath:provider.xml")
+@Controller
 @SpringBootApplication
 public class Provider {
+	
+	@Autowired
+	private UserService userService;
+	
+    @RequestMapping("/home")
+    @ResponseBody
+    String home() {
+    	String str = userService.sayHello("jerry");
+        return "提供方："+str;
+    }
 
 	public static void main(String[] args) throws IOException {
         /*ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("provider.xml");
@@ -18,7 +35,7 @@ public class Provider {
         context.start();
         System.out.println("服务已经启动...");
         System.in.read();*/
-		SpringApplication.run(Provider.class, args);
+		SpringApplication.run(Provider.class,  "--server.port=8081");
     }
 	
 }
